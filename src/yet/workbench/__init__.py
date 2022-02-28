694015772888108363e64b0dd047b9a28a00d075
Ios Cruz <iosacruz@fb.com>
2022-02-28 15:23:06 -0800

    Sync of changes

--- src/yet/workbench/__init__.py
+++ src/yet/workbench/__init__.py
@@ -1,19 +1,21 @@
-from yet.workbench import Function
-from yet.workbench.digilent import *
-from yet.workbench.siglent import Sds1104xeOscilloscope
+from yet.workbench.digilent import DwfDevice
+from yet.workbench.digilent import DwfOscilloscope
+from yet.workbench.siglent import Sds1kOscilloscope
 
 class Device(Object):
+    classes = [DwfDevice]
     def __init__(self):
 
-    def confiure(self):
+    def configure(self, config):
 
 class Instrument(Object):
+    classes = [Oscilloscope, SpiMaster]
     def __init__(self):
 
-    def configure():
+    def configure(self, config):
 
 class Oscilloscope(Instrument):
-    classes = [DwfOscilloscope, Sds1104xeOscilloscope]
+    classes = [DwfOscilloscope, Sds1kOscilloscope]
     def __init__(self):
 
 class SpiMaster(Instrument):
